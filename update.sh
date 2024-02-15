0b469339a69935c19b4ed423fcbf4d498f152d7d
izzstores <158043117+izzstores@users.noreply.github.com>
2024-02-16 05:56:49 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 G="\033[1;32m"
 N="\e[0m"
+history -c
 echo -e "${G}===================[ UPDATE MENU ]=====================${N}"
 sleep 3
 cd
@@ -10,7 +11,7 @@ chmod +x menu/*
 mv menu/* /usr/local/sbin/
 rm -rf menu menu.zip
 clear
-echo -e "${G}======================[ INSTALL SUKSES ]=======================${N}"
+echo -e "${G}======================[ UPDATE SUKSES ]=======================${N}"
 echo ""
 echo " wait 5 second , To Menu "
 sleep 5
